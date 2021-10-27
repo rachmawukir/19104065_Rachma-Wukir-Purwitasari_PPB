cd58b03f39595edca46aa284edbf11db53cef6e0
rachmawukir <72696891+rachmawukir@users.noreply.github.com>
2021-10-27 14:24:54 +0700

    Add files via upload

---
+++ Praktikum 1/StateFor.kt
@@ -0,0 +1,12 @@
+fun StateFor () {
+    val ranges = 1.rangeTo(10) step 3
+
+    //i => variabel yang diapakai di dalam loop
+    //ranges => array
+
+    val ranges = 1.rangeTo(10) step 3
+
+    for (i in ranges ) {
+        println("value is $i!")
+    }
+}
