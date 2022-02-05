640cb651d071c46dd8e1e42d4bbe34e1c5d88da6
rachmawukir <72696891+rachmawukir@users.noreply.github.com>
2022-02-05 14:06:01 +0700

    Add files via upload

---
+++ Praktikum 9/app/src/main/java/id/rachma_19104065/myapplication/SettingPreference.kt
@@ -0,0 +1,35 @@
+package id.rachma_19104065.myapplication
+
+import android.content.Context
+
+class SettingPreference (context: Context){
+    companion object {
+        private const val PREFS_NAME = "setting_pref"
+        private const val NAME = "name"
+        private const val EMAIL = "email"
+        private const val AGE = "age"
+        private const val PHONE_NUMBER = "phone"
+        private const val THEME = "theme"
+    }
+    private val preferences = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
+    //Fungsi ini digunakan untuk menyimpan value ke shared preference berdasarkan masing-masing key
+    fun setSetting(value: SettingModel) {
+        val editor = preferences.edit()
+        editor.putString(NAME, value.name)
+        editor.putString(EMAIL, value.email)
+        editor.putInt(AGE, value.age)
+        editor.putString(PHONE_NUMBER, value.phoneNumber)
+        editor.putBoolean(THEME, value.isDarkTheme)
+        editor.apply()
+    }
+    //Fungsi ini digunakan untuk membaca value shared preference yang telah tersimpan berdasarkan masing-masing key
+    fun getSetting(): SettingModel {
+        val model = SettingModel()
+        model.name = preferences.getString(NAME, "")
+        model.email = preferences.getString(EMAIL, "")
+        model.age = preferences.getInt(AGE, 0)
+        model.phoneNumber = preferences.getString(PHONE_NUMBER, "")
+        model.isDarkTheme = preferences.getBoolean(THEME, false)
+        return model
+    }
+}
